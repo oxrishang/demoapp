37fa5cb54f37f63591b0804f2123754e564fa27c
rishang <rishang@openxcellinc.com>
2022-04-28 11:23:54 +0530

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+import uvicorn
+
+
+if __name__ == "__main__":
+    uvicorn.run(host="0.0.0.0", port=5000, log_level="info")
